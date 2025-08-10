382ed19dc8bbaf409d0a2820d83b8d6aae7fb7ec
utpolodekaryoronno <info.utpolodekary@gmail.com>
2025-08-10 20:32:44 +0600

    implement UI

---
+++ dbDiagram.php
@@ -0,0 +1,49 @@
+
+Table students{
+  id integer [primary key]
+  name varchar
+  email varchar [unique]
+  phone varchar
+  studentid varchar
+  photo varchar
+  address varchar
+  created_at datetime
+  updated_at datetime
+}
+
+Table books{
+  id integer [primary key]
+  title varchar
+  author varchar
+  cover varchar
+  isbn integer
+  copies integer [default: 5]
+  available_copy integer
+  created_at datetime
+  updated_at datetime
+}
+
+Table borrowings{
+  id integer [primary key]
+  student_id int
+  book_id int
+  issue_date datetime
+  return_date datetime
+  status varchar [default: 'pending'] // pending, return, overdue
+  created_at datetime
+  updated_at datetime
+}
+
+Table reservations{
+  id integer [primary key]
+  student_id int
+  book_id int
+  borrowing_id int
+  status verchar [default: 'pending'] // pending, available
+  created_at datetime
+  updated_at datetime
+}
+
+Ref borrowings: students.id > borrowings.student_id
+Ref borrowings: books.id > borrowings.book_id
+Ref reservations: borrowings.id > reservations.borrowing_id
